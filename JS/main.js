501278f18d1215b21dc87c4dd68ae37471edec82
2e2o77 <zemad2373@gmail.com>
2024-11-28 14:17:47 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,44 @@
+document.querySelectorAll('.product-item').forEach(item => {
+    item.addEventListener('mouseenter',() => {
+        item.querySelector('img').style.transform = 'translateX(0%)';
+        const text = item.querySelector('.text');
+        text.style.left = '10px';
+        text.style.opacity = '1';
+        text.style.backgroundColor = 'rgb(221, 218, 218)';
+        text.style.border = '2px solid rgb(221, 218, 218)';
+        text.style.borderRadius = '10px';
+    })
+    item.addEventListener('mouseleave',() => {
+        item.querySelector('img').style.transform = 'translateX(0%) ';
+        item.querySelector('img').style.transition = 'all 0.4s'
+        const text = item.querySelector('.text');
+        text.style.left = '200px';
+        text.style.opacity = '0';
+    })
+})
+
+const div = document.querySelector('.product-item');
+const text = div.querySelector('.text');
+div.addEventListener('mouseenter',() => {
+    text.style.transform = 'translateX(-10%)'
+    div.addEventListener('mouseenter', ()=> {
+        text.style.transform = 'translateX(0)';
+    })
+});
+
+function validation(){
+    let fullName = document.getElementById('fullName').value;
+    let email = document.getElementById('email').value;
+    let supject = document.getElementById('supject').value;
+    let message = document.getElementById('message').value;
+
+    if (fullName===""||email===""||supject===""||message===""){
+        alert("Please Fall The Field")
+        return false
+    }
+    if (email.indexOf("@")===-1||email.indexOf(".")===-1){
+        alert("Please Write Your Email Correct")
+        return false
+    }
+    return true
+}
